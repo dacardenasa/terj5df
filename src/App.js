dab5b98829b71dd598e1945af2eb7bbb16964ce4
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-06 09:37:27 -0500

    Update var names in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
 
 class App extends Component {
+  constructor(){
+    super();
+    this.firstName = React.createRef();
+    this.lastName = React.createRef();
+    this.state = {
+      guests: []
+    }
+  }
+
+  handleSubmit(e){
+    let oldGuests = this.state.guests;
+    let newGuest = {
+      name: this.firstName.current.value, 
+      lastName: this.lastName.current.value 
+    }  
+    this.setState({
+      guests: [...oldGuests, newGuest]
+    });
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)} >
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  ref={this.firstName}
+                  type="text" 
+                  className="form-control" 
+                  name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  ref={this.lastName}
+                  type="text" 
+                  className="form-control" 
+                  name="last-name" />
               </div>
 
               <div className="action">
@@ -30,7 +63,9 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.guests.map((guest, index) =>
+                  <Row key={index} name={guest.name} lastName={guest.lastName} />
+                )}
               </tbody>
             </table>
           </div>
@@ -38,6 +73,20 @@ class App extends Component {
       </div>
     )
   }
+}
+
+const Row = (props) => {
+  return(
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.lastName}</td>
+    </tr>
+  );
+}
+
+Row.defaultProps = {
+  name: 'Marty',
+  lastName: 'McFly'    
 }
 
 export default App
