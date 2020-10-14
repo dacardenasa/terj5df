f5a19afb1fc4f4c5ed85083ac7d3742e4f07486a
dacardenasa <diego.cardenasaleg@gmail.com>
2020-10-13 21:01:54 -0500

    Update class component to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,95 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      guests: []
-    }
-  }
+function App(){
 
-  handleChangeFirstName(event){
-    this.setState({
-      firstName: event.target.value,
-    });
-  }
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [guests, setGuests] = useState([]);
 
-  handleChangeLastName(event){
-    this.setState({
-      lastName: event.target.value
-    });
-  }
-
-  handleSubmit(e){
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let oldGuests = this.state.guests;
-    let newGuest = {
-      firstName: this.state.firstName, 
-      lastName: this.state.lastName
-    }  
-    this.setState({
-      guests: [...oldGuests, newGuest]
-    });
+    setGuests([...guests,...[{ firstname, lastname }]]);
+    setFirstName("");
+    setLastName("");
   }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <form onSubmit={this.handleSubmit.bind(this)} >
-              <div className="form-group">
-                <label htmlFor="first-name">Nombre</label>
-                <input
-                  onChange={this.handleChangeFirstName.bind(this)}
-                  value={this.state.firstName}
-                  type="text" 
-                  className="form-control" 
-                  name="first-name" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="last-name">Apellido</label>
-                <input
-                  onChange={this.handleChangeLastName.bind(this)}
-                  value={this.state.lastName}
-                  type="text" 
-                  className="form-control" 
-                  name="last-name" />
-              </div>
-
-              <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
-              </div>
-            </form>
-
-            <table className="table bordered-table table-striped">
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Apellido</th>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 col-sm-offset-3">
+          <form onSubmit={ e => handleSubmit(e) } >
+            <div className="form-group">
+              <label htmlFor="first-name">Nombre</label>
+              <input
+                onChange={ e => setFirstName(e.target.value) }
+                value= { firstname }
+                type="text" 
+                className="form-control" 
+                name="first-name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="last-name">Apellido</label>
+              <input
+                onChange={ e => setLastName(e.target.value) }
+                value={ lastname }
+                type="text" 
+                className="form-control" 
+                name="last-name" />
+            </div>
+            <div className="action">
+              <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+            </div>
+          </form>
+          <table className="table bordered-table table-striped">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+              </tr>
+            </thead>
+            <tbody>
+            {guests.map((guest, index) =>
+                <tr key={index}>
+                  <td>{guest.firstname}</td>
+                  <td>{guest.lastname}</td>
                 </tr>
-              </thead>
-              <tbody>
-              {this.state.guests.map((guest, index) =>
-                  <tr key={index}>
-                    <td>{guest.firstName}</td>
-                    <td>{guest.lastName}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
